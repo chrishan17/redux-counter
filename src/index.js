ef9871096bc727ca41a0d46a5ccf96a62255647b
Chris Han <hxljustdoit@gmail.com>
2018-04-12 23:52:06 +0800

    add middleware logger

--- src/index.js
+++ src/index.js
@@ -2,11 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CounterContainer from './components/CounterContainer';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import reducer from './reducers/counter'
 import { Provider } from 'react-redux'
 
-const store = createStore(reducer, { counter: 0 }, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+const logger = store => next => action => {
+  console.log('before dispatch:', store.getState())
+  next(action)
+  console.log('after dispatch:', store.getState())
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(reducer, { counter: 0 }, composeEnhancers(applyMiddleware(
+  logger,
+  )))
 
 ReactDOM.render(
   <Provider store={store}>
